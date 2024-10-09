381ee2ea38de020007023c555606dfb2e600fa62
usmanali18 <usmanbalochhh18@gmail.com>
2024-10-09 08:25:55 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+
+function change_image(image){
+
+                 var container = document.getElementById("main-image");
+
+                container.src = image.src;
+            }
+
+
+            document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
+       });
